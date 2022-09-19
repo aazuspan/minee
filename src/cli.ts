0bdc7ad11959f4d9afd8947966f72c58504a341a
Aaron Zuspan <aa.zuspan@gmail.com>
2022-09-19 13:21:40 -0700

    Make entry an option instead of a required argument to support config files. Implement config files (#8).
    Recompile

--- src/cli.ts
+++ src/cli.ts
@@ -3,19 +3,59 @@
 import chalk from 'chalk'
 import tree from 'terminal-tree'
 import { Command } from 'commander'
-import path from 'path'
+import fs from "fs";
 import * as errors from './errors.js'
 import { bundleModule } from './bundle.js'
+
+type Config = {
+  entry: string | undefined;
+  dest: string | undefined;
+  noHeader: boolean;
+};
+
+/**
+ * Load bundling options from a local config file, swapping in defaults for missing options.
+ * @returns {Config} The configuration settings for bundling.
+ */
+function loadConfig(): Config {
+  const defaultConfig: Config = {
+    entry: undefined,
+    dest: undefined,
+    noHeader: false,
+  }
+
+  try {
+    const config = JSON.parse(fs.readFileSync(".minee.json", "utf8"));
+    return { ...defaultConfig, ...config };
+  } catch (err) {
+    return defaultConfig;
+  }
+}
+
 
 new Command()
   .name('minee')
   .version('0.0.5')
   .description('📦 Earth Engine module bundler.')
-  .arguments('<entry>')
+  .option('-e, --entry <path>', 'The path to the module entry point, e.g. users/username/repository:module.')
   .option('-d --dest <path>', 'The local file path to write the bundled file.')
   .option('--no-header', 'Drop header information from the bundled file.')
-  .action(async (entry: string, options: { dest: string, header: boolean }) => {
-    await runBundler(entry, options.dest, !options.header)
+  .action(async (options: { entry: string, dest: string, header: boolean }) => {
+    const config = loadConfig();
+    const entryPath = options.entry || config.entry;
+    const destPath = options.dest || config.dest;
+    const noHeader = options.header === false || config.noHeader;
+
+    if (entryPath === undefined) {
+      console.log(`
+      ${chalk.red(
+        'No entry point specified!'
+      )} Please specify an entry point using the -e or --entry options, or by adding an 'entry' property to your .minee.json configuration file.
+      `)
+      return
+    }
+
+    await runBundler(entryPath, destPath, noHeader)
   })
   .showHelpAfterError()
   .parse(process.argv)
@@ -30,15 +70,27 @@ new Command()
  */
 async function runBundler (
   entry: string,
-  dest: string,
+  dest: string | undefined,
   noHeader = false
 ): Promise<void> {
+  let name
+  try {
+    name = entry.split(':')[1].split('/').pop()
+    dest = dest || `./${name}.bundled.js`
+    console.log(`Bundling ${chalk.blue(entry)} to ${chalk.yellow(dest)}...\n`)
+  }
+  catch (err) {
+    console.log(`
+      ${chalk.red(
+        'Invalid entry point!'
+      )} '${entry}' does not match the required format 'users/username/repository:module'.
+      `)
+    return
+  }
+
   try {
     const bundled = await bundleModule(entry, { noHeader })
-    if (dest === undefined) {
-      dest = path.resolve(`${bundled.entry.name}.bundled.js`)
-    }
-    const moduleTree = tree(bundled.entry.dependencyTree({ pretty: false }), {
+    const moduleTree = tree(bundled.entry.dependencyTree({ pretty: true }), {
       symbol: false,
       highlight: false,
       padding: 4
